8276b3cbca91edc96aa20d73da3a0e503a23c4d6
Nathan Otto <ottoetc@gmail.com>
2016-01-22 09:57:05 -0800

    add spec testing for divisible by three

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var pingPong = function(userNumber) {
-  if((userNumber % 3 !== 0) || (userNumber % 5 !== 0) || (userNumber % 15 !== 0)) {
+  if(userNumber % 3 === 0) {
+    return "ping";
+    
+  }
+  else if((userNumber % 5 !== 0) || (userNumber % 15 !== 0)) {
     return userNumber;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('pingPong', function() {
   it("will return the number if it is not divisible by 3, 5, or 15", function() {
     expect(pingPong(7)).to.equal(7);
   });
+  it("will return 'ping' if the number is divisible by 3", function() {
+    expect(pingPong(3)).to.equal("ping");
+  });
+  
 });
