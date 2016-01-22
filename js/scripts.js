f15483875c21c1a4dac743cbffc010fb7c8f17d0
Nathan Otto <ottoetc@gmail.com>
2016-01-22 11:12:05 -0800

    change specs to work with an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
 var pingPong = function(userNumber) {
-  var output;
+  var output = [];
   var loopCounter = 0;
   while(loopCounter < userNumber) {
     loopCounter = loopCounter + 1;
     if(loopCounter % 15 === 0) {
-      output = "pingpong";
+      output.push("pingpong");
     }
     else if(loopCounter % 5 === 0) {
-      output = "pong";
+      output.push("pong");
     }
     else if(loopCounter % 3 === 0) {
-      output = "ping";
+      output.push("ping");
     }
     else {
-      output = loopCounter;
+      output.push(loopCounter);
     }
-    console.log(output);
+
   }
+  console.log(output);
   return output;
-
-
-
 };
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var userInput = $("input#number").val();
+    var pingPongOutput = pingPong(userInput);
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
 describe('pingPong', function() {
-  it("will return the number if it is not divisible by 3, 5, or 15", function() {
-    expect(pingPong(7)).to.equal(7);
+  it("will return the numbers if they are not divisible by 3, 5, or 15", function() {
+    expect(pingPong(2)).to.eql([1, 2]);
   });
   it("will return 'ping' if the number is divisible by 3", function() {
-    expect(pingPong(3)).to.equal("ping");
+    expect(pingPong(3)).to.eql([1, 2, "ping"]);
   });
   it("will return 'pong' if the number is divisible by 5", function() {
-    expect(pingPong(5)).to.equal("pong");
+    expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"]);
   });
   it("will return 'pingpong' if the number is divisible by 15", function() {
-    expect(pingPong(15)).to.equal("pingpong");
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]);
   });
 });
